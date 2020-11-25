e3af0f2e800f639561945ab6ca81cbfcaa6a5cc7
Aniket Bharsakale <aniketbharsakale@gmail.com>
2020-11-26 00:24:53 +0530

    update prototype content

--- dev-ed-js-basics/19-es5-prototypes.js
+++ dev-ed-js-basics/19-es5-prototypes.js
@@ -1,21 +1,24 @@
+// ES5 Prototypes
+
 function Book(name, year, author, count) {
+    // this is a constructor function
     this.name = name;
     this.year = year;
     this.author = author;
     this.count = count;
 }
 
-// Syntax to create a function constructor | this is recommended approach
+// Syntax to create a method for a constructor function
 Book.prototype.getSummary = function(){
     return `${this.author} wrote his book no. ${this.count} named ${this.name} in year ${this.year}!`;
 }
 
-// object creation
+// object instantiaition
 const shadowed = new Book("Shadowed", "2024", "Aniket Bharsakale", 1);
+shadowed.getSummary();
+
 const theComeBack = new Book("The Come Back", "2026", "Aniket Bharsakale", 2);
 
-// invoking the prototype category methods of objects
-shadowed.getSummary();
 // these methods are now in the prototype section of the object & not in the field attribute section
 // hence it makes it loose coupled & all objects only have name, year, author count etc attributes & functions are kept separate
 theComeBack.getSummary();
